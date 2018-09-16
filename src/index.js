e4ccc05f41b2c1e2f58a9f65eb79b195623bb273
Alex Brown <alexanderbrown.info@gmail.com>
2018-09-16 09:43:38 +0100

    plug sagas and redux into react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSageMiddleware from 'redux-saga';
+
 import './index.css';
 import App from './containers/App';
+import reducer from './redux/reducers'
+import searchSaga from './redux/sagas/sagas'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSageMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(searchSaga)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
